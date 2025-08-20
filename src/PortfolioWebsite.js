beceaeea8aff0117e0ac4dd6265d28383868cd61
habiburrhaman589-a11y <habiburrhaman589@gmail.com>
2025-08-20 10:32:07 +0600

    Create PortfolioWebsite.js

---
+++ src/PortfolioWebsite.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PortfolioWebsite() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-600 via-pink-500 to-red-500 text-white">
+      {/* তোমার JSX কোড এখানে থাকবে */}
+    </div>
+  );
+}
