520112d831465acec0b5f6d0b220d6179151145e
habiburrhaman589-a11y <habiburrhaman589@gmail.com>
2025-08-20 10:21:41 +0600

    Update App.js

--- App.js
+++ App.js
@@ -107,3 +107,14 @@ export default function PortfolioWebsite() {
     </div>
   );
 }
+
+
+
+import React from 'react';
+import PortfolioWebsite from './PortfolioWebsite';
+
+function App() {
+  return <PortfolioWebsite />;
+}
+
+export default App;
